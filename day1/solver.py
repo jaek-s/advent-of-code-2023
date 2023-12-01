01373ad3a743267535713d2f73d01b904165d31c
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-12-01 00:47:28 -0700

    first failed attempt at the second puzzle

--- day1/solver.py
+++ day1/solver.py
@@ -5,22 +5,57 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 regex_get_first_digit = re.compile("^.*?(\\d)")
 
-def solve_puzzle():
-    sum = 0
+NUMBER_STRINGS = {
+    "one":"1",
+    "two":"2",
+    "three":"3",
+    "four":"4",
+    "five":"5",
+    "six":"6",
+    "seven":"7",
+    "eight":"8",
+    "nine":"9",
+}
+
+def replace_letter_digits(input: str):
+    for (letter_digit, real_digit) in NUMBER_STRINGS.items():
+        input = input.replace(letter_digit, real_digit)
+    return input
+
+def get_input_lines():
     with open(current_dir + "/input.txt") as file:
         for line in file.readlines():
-            stripped = line.strip()
-            reversed = stripped[::-1]
-            first_digit_match = regex_get_first_digit.match(stripped)
-            last_digit_match = regex_get_first_digit.match(reversed)
+            yield line.strip()
 
-            if not first_digit_match or not last_digit_match:
-                continue
+def get_calibration_value(line: str) -> int:
+    first_digit_match = regex_get_first_digit.match(line)
+    last_digit_match = regex_get_first_digit.match(line[::-1])
 
-            line_digits = int(f"{first_digit_match.group(1)}{last_digit_match.group(1)}")
-            sum += line_digits
+    if not first_digit_match or not last_digit_match:
+        return 0
+
+    return int(f"{first_digit_match.group(1)}{last_digit_match.group(1)}")
+
+
+def solve_first_puzzle():
+    sum = 0
+
+    for line in get_input_lines():
+        sum += get_calibration_value(line)
 
     return sum
 
+def solve_second_puzzle():
+    sum = 0
+
+    for line in get_input_lines():
+        converted_line = replace_letter_digits(line)
+        sum += get_calibration_value(converted_line)
+
+    return sum
+
+
+
 if __name__ == "__main__":
-    print(solve_puzzle())
+    print(f"first puzzle solution: {solve_first_puzzle()}")
+    print(f"second puzzle solution: {solve_second_puzzle()}")
