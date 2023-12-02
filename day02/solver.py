e1235b7a774e08d6a9043c1975fcd8a028aad0a6
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-12-02 02:02:37 -0700

    solve day 2 puzzle 2

--- day02/solver.py
+++ day02/solver.py
@@ -1,13 +1,29 @@
 from helpers import get_input_lines
 
-def get_games_from_input():
+
+def get_games_from_input() -> dict[int, list[dict[str, int]]]:
+    parsed_games = {}
     for line in get_input_lines("02"):
         game_id, game = line.split(": ")
 
         game_id = int(game_id.replace("Game ", ""))
-        game = game.split("; ")
 
-        yield game_id, game
+        # I'd probably be rasing my eyebrows if I saw a combination of list and dict comprehensions like this in production code.
+        # It does make the code below a _little_ bit cleaner, though.
+
+        # This gives a list of dicts that looks like [{ red: 1, green: 2, blue: 3}]
+        game = [
+            {
+                color: int(count)
+                for (count, color) in [draw.split() for draw in round.split(", ")]
+            }
+            for round in game.split("; ")
+        ]
+
+        parsed_games[game_id] = game
+
+    return parsed_games
+
 
 # ---------------------------------------------------------------------------- #
 #                                 first puzzle                                 #
@@ -18,27 +34,21 @@ MAX_GREEN = 13
 MAX_BLUE = 14
 
 
-def game_is_possible(game: list[str]):
+def game_is_possible(game: list[dict[str, int]]):
     for round in game:
-        draws = round.split(", ")
-        counts = {
-            color: int(count) for (count, color) in [draw.split() for draw in draws]
-        }
-
         if (
-            counts.get("red", 0) > MAX_RED
-            or counts.get("green", 0) > MAX_GREEN
-            or counts.get("blue", 0) > MAX_BLUE
+            round.get("red", 0) > MAX_RED
+            or round.get("green", 0) > MAX_GREEN
+            or round.get("blue", 0) > MAX_BLUE
         ):
             return False
 
     return True
 
 
-def solve_first_puzzle():
+def solve_first_puzzle(games: dict[int, list[dict[str, int]]]):
     sum = 0
-    for (game_id, game) in get_games_from_input():
-
+    for game_id, game in games.items():
         if not game_is_possible(game):
             continue
 
@@ -52,7 +62,32 @@ def solve_first_puzzle():
 # ---------------------------------------------------------------------------- #
 
 
+def get_power_of_min_set_of_cubes(game):
+    red = 0
+    green = 0
+    blue = 0
+
+    for round in game:
+        if round.get("red", 0) > red:
+            red = round.get("red", 0)
+        if round.get("green", 0) > green:
+            green = round.get("green", 0)
+        if round.get("blue", 0) > blue:
+            blue = round.get("blue", 0)
+
+    return red * green * blue
+
+
+def solve_second_puzzle(games: dict[int, list[dict[str, int]]]):
+    sum = 0
+
+    for game in games.values():
+        sum += get_power_of_min_set_of_cubes(game)
+
+    return sum
+
 
 if __name__ == "__main__":
-    print(f"first puzzle solution: {solve_first_puzzle()}")
-    # print(f"second puzzle solution: {solve_second_puzzle()}")
+    games = get_games_from_input()
+    print(f"first puzzle solution: {solve_first_puzzle(games)}")
+    print(f"second puzzle solution: {solve_second_puzzle(games)}")
