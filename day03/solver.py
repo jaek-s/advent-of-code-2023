4dbd86280a9ba072063f457a78aab55ea0c80e03
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-12-03 23:16:22 -0700

    solve second puzzle

--- day03/solver.py
+++ day03/solver.py
@@ -1,24 +1,42 @@
 import re
-from pprint import pprint
 
 from helpers import open_input
+
+match_digits = re.compile(r"\d+")  # match any amount of digits
+
+
+def get_input_lines_with_next_and_prev():
+    with open_input("03") as file:
+        lines = file.read().strip().split("\n")
+
+        for index in range(0, len(lines)):
+            current_line = lines[index]
+            prev_line = lines[index - 1] if index > 0 else ""
+            next_line = lines[index + 1] if index < len(lines) - 1 else ""
+
+            yield current_line, prev_line, next_line
+
 
 # ---------------------------------------------------------------------------- #
 #                                 first puzzle                                 #
 # ---------------------------------------------------------------------------- #
 
-match_part_numbers = re.compile(r"\d+")  # match any amount of digits
+
 match_symbols = re.compile(r"[^\d\.]")  # match anything other than a digit or a period
 
-
-def get_part_num_sum_for_line(index: int, lines: list[str]):
+def solve_first_puzzle():
     sum = 0
 
-    current_line = lines[index]
-    prev_line = lines[index - 1] if index > 0 else ""
-    next_line = lines[index + 1] if index < len(lines) - 1 else ""
+    for current_line, prev_line, next_line in get_input_lines_with_next_and_prev():
+        sum += get_part_num_sum_for_line(current_line, prev_line, next_line)
 
-    for match in match_part_numbers.finditer(current_line):
+    return sum
+
+
+def get_part_num_sum_for_line(current_line: str, prev_line: str, next_line: str):
+    sum = 0
+
+    for match in match_digits.finditer(current_line):
         match_start, match_end = match.span()
 
         search_start = match_start - 1 if match_start > 0 else 0
@@ -45,23 +63,76 @@ def get_part_num_sum_for_line(index: int, lines: list[str]):
     return sum
 
 
-def solve_first_puzzle():
-    sum = 0
-
-    with open_input("03") as file:
-        lines = file.read().strip().split("\n")
-
-        for index in range(0, len(lines)):
-            sum += get_part_num_sum_for_line(index, lines)
-
-    return sum
-
-
 # ---------------------------------------------------------------------------- #
 #                                 second puzzle                                #
 # ---------------------------------------------------------------------------- #
 
 
+match_gears = re.compile(r"\*")
+match_part_number_at_end = re.compile(r"\d+$")
+match_part_number_at_start = re.compile(r"^\d+")
+match_non_digits = re.compile(r"[^\d]")
+
+
+def solve_second_puzzle():
+    sum = 0
+
+    for current_line, prev_line, next_line in get_input_lines_with_next_and_prev():
+        sum += get_gear_ratio_sum_for_line(current_line, prev_line, next_line)
+
+    return sum
+
+
+def get_gear_ratio_sum_for_line(current_line: str, prev_line: str, next_line: str):
+    sum = 0
+
+    for match in match_gears.finditer(current_line):
+        match_start, match_end = match.span()
+        part_nums = []
+
+        left_of_match = current_line[:match_start]
+        part_on_left = match_part_number_at_end.search(left_of_match)
+
+        if part_on_left:
+            part_nums.append(part_on_left[0])
+
+        right_of_match = current_line[match_end:]
+        part_on_right = match_part_number_at_start.search(right_of_match)
+
+        if part_on_right:
+            part_nums.append(part_on_right[0])
+
+        part_nums += get_parts_above_or_below(prev_line, match_start)
+        part_nums += get_parts_above_or_below(next_line, match_start)
+
+
+        # two number matches need to be found or else continue
+        if len(part_nums) != 2:
+            continue
+
+        sum += int(part_nums[0]) * int(part_nums[1])
+
+    return sum
+
+
+def get_parts_above_or_below(line: str, gear_position: int) -> list[str]:
+    part_numbers_string = line[gear_position: gear_position + 1]
+
+    left_half = line[:gear_position]
+    left_match = match_part_number_at_end.search(left_half)
+
+    if left_match:
+        part_numbers_string = f"{left_match[0]}{part_numbers_string}"
+
+    right_half = line[gear_position + 1:]
+    right_match = match_part_number_at_start.search(right_half)
+
+    if right_match:
+        part_numbers_string = f"{part_numbers_string}{right_match[0]}"
+
+    return [part for part in match_non_digits.split(part_numbers_string) if part]
+
+
 if __name__ == "__main__":
     print(f"first puzzle solution: {solve_first_puzzle()}")
-    # print(f"second puzzle solution: {solve_second_puzzle()}")
+    print(f"second puzzle solution: {solve_second_puzzle()}")
