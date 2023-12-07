0c9bdca135356b126c38cf05c3d84f54d68d64c7
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-12-06 20:43:43 -0700

    fix bugs when checking if a solver exists

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ def main(day: int):
 
     print(f"[bold]Day {day:0>2}[/bold]")
 
-    if not solvers.solve_first_puzzle and not solvers.solve_first_puzzle:
-        print(f"Please add a [blue]`solve_first_puzzle`[/blue] and/or [blue]solve_second_puzzle[/blue] function to [blue]./day{day:0>2}/solvers.py[/blue]")
+    if not hasattr(solvers, "puzzle1") and not hasattr(solvers, "puzzle2"):
+        print(f"Please add a [blue]`puzzle1`[/blue] and/or [blue]puzzle2[/blue] function to [blue]./day{day:0>2}/solvers.py[/blue]")
         return
 
-    if solvers.solve_first_puzzle:
-        print(f"puzzle 1 solution: [bold green]{solvers.solve_first_puzzle()}[/bold green]")
+    if hasattr(solvers, "puzzle1"):
+        print(f"puzzle 1 solution: [bold green]{solvers.puzzle1()}[/bold green]")
     else:
-        print(f"puzzle 1 solution: [bold red]No solver found![/bold red] Please create a [blue]solve_first_puzzle[/blue] function")
+        print(f"puzzle 1 solution: [bold red]No solver found![/bold red] Please create a [blue]puzzle1[/blue] function")
 
-    if solvers.solve_first_puzzle:
-        print(f"puzzle 2 solution: [bold green]{solvers.solve_second_puzzle()}[/bold green]")
+    if hasattr(solvers, "puzzle2"):
+        print(f"puzzle 2 solution: [bold green]{solvers.puzzle2()}[/bold green]")
     else:
-        print(f"puzzle 1 solution: [bold red]No solver found![/bold red] Please create a [blue]solve_second_puzzle[/blue] function")
+        print(f"puzzle 2 solution: [bold red]No solver found![/bold red] Please create a [blue]puzzle2[/blue] function")
 
 
 if __name__ == "__main__":
